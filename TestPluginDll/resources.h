409f76e95041b64c1e2823b1ef3dce562cdc9eac
David Noah Peinsipp <werti1304@gmail.com>
2019-11-17 21:21:46 +0100

    Changed name; Added Resources; Made a few tweaks

---
+++ TestPluginDll/resources.h
@@ -0,0 +1,6 @@
+#pragma once
+
+static const char* icon_1_png = "1.png";
+static const char* icon_2_png = "2.png";
+static const char* icon_3_png = "3.png";
+static const char* icon_t_png = "t.png";
